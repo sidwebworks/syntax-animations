a5191637a7e28884e11e46242feb0aa96d839279
Sidwebworks <58144379+sidwebworks@users.noreply.github.com>
2025-10-03 01:48:15 +0530

    fix: isolate http cache by origin

--- app/lib/cache.ts
+++ app/lib/cache.ts
@@ -1,8 +1,11 @@
 import LocalForage from "localforage";
 import type { SlideID, TimelineSlide } from "./store";
 
+const storeName = typeof window != "undefined" ? window.location.hostname : "";
+
 export const httpCache = LocalForage.createInstance({
   name: "http_cache",
+  storeName,
 });
 
 export enum ProjectDataKeys {
@@ -14,4 +17,5 @@ export type DehydratedSlides = Record<SlideID, TimelineSlide & { code: string }>
 
 export const projectData = LocalForage.createInstance({
   name: "project_data",
+  storeName,
 });
